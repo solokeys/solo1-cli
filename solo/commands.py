7883b0b30f771eaccfd0729e2d6ccfbf2abf1393
Nicolas Stalder <n@stalder.io>
2019-02-27 02:38:58 +0100

    Do everything in the PROBE command

--- solo/commands.py
+++ solo/commands.py
@@ -27,7 +27,7 @@ class SoloBootloader:
     HIDCommandEnterBoot = 0x51
     HIDCommandEnterSTBoot = 0x52
     HIDCommandRNG = 0x60
-    HIDCommandCalculateSHA512 = 0x71
+    HIDCommandProbe = 0x70
 
     TAG = b"\x8C\x27\x90\xf6"
 
