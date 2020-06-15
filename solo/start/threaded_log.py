11741e92f749af6f9ed22ed47279835a833c1140
Szczepan Zalega <szczepan@nitrokey.com>
2020-06-15 11:11:54 +0200

    Add/correct license statements

--- solo/start/threaded_log.py
+++ solo/start/threaded_log.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python3
-
+# -*- coding: utf-8 -*-
 
 """
-Copyright (C) 2020 Nitrokey Gmbh
+Copyright 2020 Nitrokey Gmbh
+
+Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+http://opensource.org/licenses/MIT>, at your option. This file may not be
+copied, modified, or distributed except according to those terms.
 """
 
 import logging
