e8371a477168aa3e784c8829f8c2075fc6338b6e
Markus Meissner <coder@safemailbox.de>
2020-07-13 16:33:17 +0200

    one more try-except; logging; user confirmation

--- pynitrokey/cli/update.py
+++ pynitrokey/cli/update.py
@@ -14,16 +14,28 @@ import sys
 import tempfile
 import json
 import time
+import logging
 
 import pynitrokey
-from fido2.ctap import CtapError
-from fido2.ctap1 import ApduError
+
+# @fixme: redundant copy from 'pynitrokey.start.upgrade_by_passwd'
+UPGRADE_LOG_FN = tempfile.NamedTemporaryFile(prefix="nitropy.log.").name
+LOG_FORMAT_STDOUT = '*** %(asctime)-15s %(levelname)6s %(name)10s %(message)s'
+LOG_FORMAT = '%(relativeCreated)-8d %(levelname)6s %(name)10s %(message)s'
+logging.basicConfig(format=LOG_FORMAT, level=logging.DEBUG, filename=UPGRADE_LOG_FN)
+logger = logging.getLogger()
+
+def local_print(message: str = '', **kwargs):
+    if message and message != '.':
+        logger.debug('print: {}'.format(message.strip()))
+    print(message, **kwargs)
 
 
 @click.command()
 @click.option("-s", "--serial", help="Serial number of Nitrokey key to target",
               default=None)
-def update(serial):
+@click.option('-y', 'yes', default=False, is_flag=True, help='agree to everything')
+def update(serial, yes):
     """Update Nitrokey key to latest firmware version."""
 
     #update_url = 'https://update.nitrokey.com/'
@@ -36,22 +48,22 @@ def update(serial):
 
     except pynitrokey.exceptions.NoSoloFoundError:
         print()
-        print("No Nitrokey key found!")
+        local_print("No Nitrokey key found!")
         print()
-        print("If you are on Linux, are your udev rules up to date?")
-        print("For more, see https://www.nitrokey.com/documentation/installation#os:linux")
+        local_print("If you are on Linux, are your udev rules up to date?")
+        local_print("For more, see https://www.nitrokey.com/documentation/installation#os:linux")
         print()
         sys.exit(1)
     except pynitrokey.exceptions.NonUniqueDeviceError:
         print()
-        print("Multiple Nitrokey keys are plugged in! Please:")
-        print("  * unplug all but one key")
+        local_print("Multiple Nitrokey keys are plugged in! Please:")
+        local_print("  * unplug all but one key")
         print()
         sys.exit(1)
     except Exception:
         print()
-        print("Unhandled error connecting to key.")
-        print("Please report via https://github.com/Nitrokey/pynitrokey/issues/")
+        local_print("Unhandled error connecting to key.")
+        local_print("Please report via https://github.com/Nitrokey/pynitrokey/issues/")
         print()
         sys.exit(1)
 
@@ -65,24 +77,39 @@ def update(serial):
             download_url = url
             break
     if download_url is None:
-        print("Failed to determine latest release")
-        return
+        local_print("Failed to determine latest release")
+        sys.exit(1)
 
     # download asset url
-    print("Downloading latest firmware")
+    local_print("Downloading latest firmware")
     tmp_dir = tempfile.gettempdir()
     fw_fn = os.path.join(tmp_dir, "fido2_firmware.json")
     with open(fw_fn, "wb") as fd:
         firmware = requests.get(download_url)
         fd.write(firmware.content)
 
+    # ask for permission
+    if not yes:
+        local_print('This will update your Nitrokey FIDO2...')
+        answer = input('Do you want to continue? [yes/no]: ')
+        local_print('Entered: "{}"'.format(answer))
+        logger.debug('Continue? "{}"'.format(answer))
+        if answer != 'yes':
+            local_print('Device is not modified. Exiting.')
+            sys.exit(1)
+
     # Ensure we are in bootloader mode
     if client.is_solo_bootloader():
-        print("Key already in bootloader mode, continuing...")
+        local_print("Key already in bootloader mode, continuing...")
     else:
-        print("Entering bootloader mode, please confirm with button on key!")
-        client.enter_bootloader_or_die()
-        time.sleep(0.5)
+        try:
+            local_print("Entering bootloader mode, please confirm with button on key!")
+            client.enter_bootloader_or_die()
+            time.sleep(0.5)
+        except Exception as e:
+            local_print("ERROR - problem switching to bootloader mode:")
+            local_print(str(e))
+            sys.exit(1)
 
     # reconnect and actually flash it...
     try:
@@ -90,10 +117,10 @@ def update(serial):
         client.use_hid()
         client.program_file(fw_fn)
     except Exception as e:
-        print("ERROR - problem flashing firmware:")
-        print(e)
+        local_print("ERROR - problem flashing firmware:")
+        local_print(str(e))
         sys.exit(1)
-    print("Congratulations, your key was updated to the latest firmware.")
+    local_print("Congratulations, your key was updated to the latest firmware.")
 
 
 
