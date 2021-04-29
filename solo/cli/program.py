fe93797d916871e5c15cb95b7b0959eb4dfb4327
Conor Patrick <conorpp94@gmail.com>
2021-04-29 11:35:52 -0700

    fix dfu with changes suggested by @enrikb

--- solo/cli/program.py
+++ solo/cli/program.py
@@ -245,11 +245,12 @@ def enter_dfu(serial):
     """
 
     p = solo.client.find(serial)
-    p.enter_st_dfu()
-    # this doesn't really work yet ;)
-    # p.reboot()
-
-    print("Please powercycle the device (pull out, plug in again)")
+    try:
+        p.enter_st_dfu()
+        print("Please powercycle the device (pull out, plug in again)")
+    except Exception as e:
+        if "wrong channel" in str(e).lower():
+            print("Command wasn't accepted by Solo.  It must be in bootloader mode first and be a 'hacker' device.")
 
 
 aux.add_command(enter_dfu)
