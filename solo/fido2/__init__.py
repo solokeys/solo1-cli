3cda1849039bb56e89846380592e9639b0dd545c
stevenwdv <stevenwdv@gmail.com>
2021-11-04 16:27:38 +0100

    Bring back HID over UDP support for simulator

---
+++ solo/fido2/__init__.py
@@ -0,0 +1,7 @@
+def force_udp_backend():
+    import fido2.hid
+    import solo.fido2.udp_backend as udp_backend
+    fido2.hid.backend = udp_backend
+    fido2.hid.list_descriptors = udp_backend.list_descriptors
+    fido2.hid.get_descriptor = udp_backend.get_descriptor
+    fido2.hid.open_connection = udp_backend.open_connection
